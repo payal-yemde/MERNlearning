b0c027243661e26a73fababd23dd377f607d5435
payal-yemde <payalyemde253@gmail.com>
2024-06-15 19:19:53 +0530

    day12.js

--- pattern assissgnment/day12.js
+++ pattern assissgnment/day12.js
@@ -19,10 +19,16 @@
 //console.log(odd)
 
 //7
-//let=[{firstName:"Nayan", lastName: "Kale", place: "Banglore" },
+
+//let users=[{firstName:"Nayan", lastName: "Kale", place: "Banglore" },
 //{ firstName: "Payal", lastName: "Yemde", place: "Banglore" }, 
 //{ firstName: "Pooja", lastName: "Mandaogade", place: "Kolkata" }
 //];
+
+//let output = users.map(function(user){
+//return (user.firstName + " " +user.lastName)
+//})
+//console.log(output )
 
 //4
 //let product =[1,2,3,4].reduce(function(acc,elem){
@@ -30,11 +36,44 @@
 //},1)
 //console.log(product)
 
-//5
-let num=[1,2,3,4,5,6,7,8,9]
-let oddaddition =num.filter(function(elem){
-  return elem%2 ==1
-}).reduce(function(acc,elem){
-  return acc+elem
-},0)
-console.log(oddaddition)
+//6
+//let num=[1,2,3,4,5,6].filter(function(elem){
+ // return elem%3 == 0
+//}).map(function(elem){
+  //return (elem*elem*elem)
+//}).reduce(function(acc,elem){
+  //return acc+ elem
+//},0)
+
+//console.log(num)
+
+//8
+
+//let output2 = users.filter(function(user){
+//return user.place == 'Banglore'
+//}).map(function(user){
+//return (user.firstName + " " + user.lastName)
+//})
+//console.log(output2)
+
+//9
+//let arr=['Coding','Room']
+//let elem=arr.map(function(elem){
+//return elem[0]
+//})
+//console.log(elem)
+
+//10
+//let arr=["apple","windows","ubuntu"]
+//let elem=arr.forEach(function(elem){
+  //console.log(elem.length)
+//})
+
+//11
+let arr1=['assignment','problem','media','upload']
+let elem1=arr1.filter(function(elem){
+return elem[0] == 'a'  || elem[elem.length-1] =='a'
+})
+
+console.log(elem1)
+
